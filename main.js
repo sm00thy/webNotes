e24c19f39e39694f5d334117631171dc304acb7e
sm00thy <ibalance5@gmail.com>
2019-01-12 00:24:05 +0100

    update

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', appstart)
-
+document.addEventListener('DOMContentLoaded', appStart)
 let notes = []
 
 function appStart(){
     const newNoteSubmit = document.querySelector('#newNoteSubmit')
     newNoteSubmit.addEventListener('click', addNewNote)
+    notes = JSON.parse(localStorage.getItem('notes')) || []
+    notes.forEach(note => {
+    addNotesToContainer(note)
+    })
 }
 
 function addNewNote() {
@@ -21,6 +24,19 @@ function addNoteToNotesContainer(note){
     const noteDateTime = new Date(note.id)
     let noteDiv = document.createElement('#note')
     noteDiv.classList.add(note)
-    noteDiv.innerHTML = '\
-    '
+    noteDiv.innerHTML = `<div class="note-title">${note.title}</div>
+    <div class="note-content">${note.content}</div>
+    <div class="note-datetime">${noteDateTime.content}</div>`
+    const nodesContainer = document.querySelector("#notesContainer")
+    nodesContainer.appendChild(noteDiv)
+}
+
+function updateLocalStorage(){
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+function note(title = '', content = ''){
+    this.title = title
+    this.content = content
+    this.id = Date.now()
 }
